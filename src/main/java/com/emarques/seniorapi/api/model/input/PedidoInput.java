36a7eabc85d10ad9b38b8454c8da413f682c185f
Carlos Eduardo <carlospereira@pactosolucoes.com.br>
2022-12-24 02:03:03 -0300

    feat: Implementando algumas exceptions

--- src/main/java/com/emarques/seniorapi/api/model/input/PedidoInput.java
+++ src/main/java/com/emarques/seniorapi/api/model/input/PedidoInput.java
@@ -1,23 +1,16 @@
 package com.emarques.seniorapi.api.model.input;
 
 import com.emarques.seniorapi.domain.model.Endereco;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 public class PedidoInput {
-
-    @PositiveOrZero
-    private BigDecimal valorTotal;
 
     @NotNull
     private Endereco enderecoEntrega;
--- src/main/java/com/emarques/seniorapi/domain/service/ProdutoService.java
+++ src/main/java/com/emarques/seniorapi/domain/service/ProdutoService.java
@@ -1,11 +1,14 @@
 package com.emarques.seniorapi.domain.service;
 
+import com.emarques.seniorapi.domain.exception.EntidadeEmUsoException;
 import com.emarques.seniorapi.domain.exception.ProdutoNaoEncontradoException;
 import com.emarques.seniorapi.domain.model.Produto;
 import com.emarques.seniorapi.domain.repository.ProdutoRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +16,9 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class ProdutoService {
+
+    private static final String MSG_PRODUTO_EM_USO
+            = "O Produto %d não pode ser removida, pois está em uso";
 
     private ProdutoRepository produtoRepository;
 
@@ -45,8 +51,15 @@ public class ProdutoService {
 
     @Transactional
     public void remover(Long produtoId){
+        try {
         produtoRepository.deleteById(produtoId);
         produtoRepository.flush();
+        } catch (EmptyResultDataAccessException e) {
+            throw new ProdutoNaoEncontradoException(produtoId);
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(
+                    String.format(MSG_PRODUTO_EM_USO, produtoId));
+        }
     }
 
     /*----- NEGOCIO -------*/
